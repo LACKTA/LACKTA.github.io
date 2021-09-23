6e9f9a4dec5870955368d7eea2133c5e917be44f
LACKTA <s6201072510089@email.kmutnb.ac.th>
2021-09-24 00:02:34 +0700

    first commit

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,3 +1,27 @@
-AOS.init({
-    duration: 1200,
-  })
+var i = 1;
+function reset() {
+  document.getElementById("reset").reset();
+}
+document.querySelector('form').onsubmit = function () {
+  const username = document.querySelector('#floatingInput').value;
+  const password = document.querySelector('#floatingPassword').value;
+
+  if (i == 5) {
+    const submit = document.querySelector('#submit');
+    alert('Connection Error');
+    submit.disabled = true;
+    return false;
+
+  } else {
+    if (username == "admin" && password == "1234") {
+      window.location.href = 'index.html';
+      return false;
+
+    } else {
+      alert('Username or Password is incorrect. Please try again, ' + String(5 - i) + ' time left.');
+      reset();
+      i++;
+      return false;
+    }
+  }
+}
